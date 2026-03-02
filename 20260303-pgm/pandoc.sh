e73f41916571cd20e7c7eede951f2403d31492a3
Hilofumi Yamamoto <hilofumi.yamamoto@gmail.com>
2026-03-03 06:48:43 +0900

    20260303-pgm updated.

--- 20260303-pgm/pandoc.sh
+++ 20260303-pgm/pandoc.sh
@@ -6,7 +6,7 @@ OUTPUT="${INPUT%.md}.html"
 
 pandoc -f markdown "$INPUT" \
   -s \
-  --mathjax \
+  --mathjax=https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js \
   -c lecture.css \
   --metadata title="Final Lecture" \
   -o "$OUTPUT"
